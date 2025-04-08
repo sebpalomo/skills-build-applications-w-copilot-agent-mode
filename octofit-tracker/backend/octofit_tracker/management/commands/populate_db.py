200943db2303b7949a400e3e3648263c3605fc9b
Sebastian Palomo Torres <114319390+sebpalomo@users.noreply.github.com>
2025-04-08 16:15:29 +0000

    Step3

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,30 +1,36 @@
 from django.core.management.base import BaseCommand
-from pymongo import MongoClient
-from octofit_tracker.test_data import get_test_data
+from octofit_tracker.test_data import test_data
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 
 class Command(BaseCommand):
-    help = 'Populate the octofit_db database with test data'
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
-        # Connect to MongoDB
-        client = MongoClient('localhost', 27017)
-        db = client['octofit_db']
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
 
-        # Drop existing collections
-        db.users.drop()
-        db.teams.drop()
-        db.activity.drop()
-        db.leaderboard.drop()
-        db.workouts.drop()
+        # Populate users
+        for user_data in test_data['users']:
+            User.objects.create(**user_data)
 
-        # Get test data
-        test_data = get_test_data()
+        # Populate teams
+        for team_data in test_data['teams']:
+            Team.objects.create(**team_data)
 
-        # Insert test data
-        db.users.insert_many(test_data['users'])
-        db.teams.insert_many(test_data['teams'])
-        db.activity.insert_many(test_data['activities'])
-        db.leaderboard.insert_many(test_data['leaderboard'])
-        db.workouts.insert_many(test_data['workouts'])
+        # Populate activities
+        for activity_data in test_data['activities']:
+            Activity.objects.create(**activity_data)
+
+        # Populate leaderboard
+        for leaderboard_data in test_data['leaderboard']:
+            Leaderboard.objects.create(**leaderboard_data)
+
+        # Populate workouts
+        for workout_data in test_data['workouts']:
+            Workout.objects.create(**workout_data)
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
--- octofit-tracker/backend/octofit_tracker/test_data.py
+++ octofit-tracker/backend/octofit_tracker/test_data.py
@@ -1,28 +1,29 @@
 from bson import ObjectId
-from datetime import timedelta
 
-def get_test_data():
-    return {
-        "users": [
-            {"_id": ObjectId(), "name": "Thor", "email": "thor@asgard.com", "age": 1500},
-            {"_id": ObjectId(), "name": "Iron Man", "email": "tony@starkindustries.com", "age": 48},
-            {"_id": ObjectId(), "name": "Hulk", "email": "bruce@banner.com", "age": 49},
-            {"_id": ObjectId(), "name": "Black Widow", "email": "natasha@shield.com", "age": 35},
-            {"_id": ObjectId(), "name": "Captain America", "email": "steve@avengers.com", "age": 105},
-        ],
-        "teams": [
-            {"_id": ObjectId(), "name": "Avengers", "members": []},
-        ],
-        "activities": [
-            {"_id": ObjectId(), "user": None, "type": "Running", "duration": 30 * 60, "calories_burned": 300},
-            {"_id": ObjectId(), "user": None, "type": "Cycling", "duration": 60 * 60, "calories_burned": 600},
-        ],
-        "leaderboard": [
-            {"_id": ObjectId(), "user": None, "score": 100},
-            {"_id": ObjectId(), "user": None, "score": 90},
-        ],
-        "workouts": [
-            {"_id": ObjectId(), "name": "Morning Yoga", "description": "A relaxing yoga session", "duration": 30},
-            {"_id": ObjectId(), "name": "Strength Training", "description": "Building muscle strength", "duration": 45},
-        ],
-    }
+# Define users first
+users = [
+    {"_id": ObjectId(), "name": "John Doe", "email": "john@example.com", "age": 30},
+    {"_id": ObjectId(), "name": "Jane Smith", "email": "jane@example.com", "age": 25},
+    {"_id": ObjectId(), "name": "Alice Johnson", "email": "alice@example.com", "age": 28},
+]
+
+# Define test data
+test_data = {
+    "users": users,
+    "teams": [
+        {"_id": ObjectId(), "name": "Team A", "members": []},
+        {"_id": ObjectId(), "name": "Team B", "members": []},
+    ],
+    "activities": [
+        {"_id": ObjectId(), "user": users[0], "type": "Running", "duration": 60, "calories_burned": 500},
+        {"_id": ObjectId(), "user": users[1], "type": "Cycling", "duration": 45, "calories_burned": 400},
+    ],
+    "leaderboard": [
+        {"_id": ObjectId(), "user": users[0], "score": 100},
+        {"_id": ObjectId(), "user": users[1], "score": 90},
+    ],
+    "workouts": [
+        {"_id": ObjectId(), "name": "Morning Yoga", "description": "A relaxing yoga session", "duration": 30},
+        {"_id": ObjectId(), "name": "Strength Training", "description": "Building muscle strength", "duration": 45},
+    ]
+}
