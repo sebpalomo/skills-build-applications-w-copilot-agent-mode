ba82189424f9a977ca5168d6f0753d6664b0784b
Sebastian Palomo Torres <114319390+sebpalomo@users.noreply.github.com>
2025-04-08 15:57:11 +0000

    Step3

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from pymongo import MongoClient
+from octofit_tracker.test_data import test_data
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Connect to MongoDB
+        client = MongoClient('localhost', 27017)
+        db = client['octofit_db']
+
+        # Drop existing collections
+        db.users.drop()
+        db.teams.drop()
+        db.activity.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
+
+        # Insert test data
+        db.users.insert_many(test_data['users'])
+        db.teams.insert_many(test_data['teams'])
+        db.activity.insert_many(test_data['activities'])
+        db.leaderboard.insert_many(test_data['leaderboard'])
+        db.workouts.insert_many(test_data['workouts'])
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
